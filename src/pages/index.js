0b3d9a842961dc3b9d0b1ce14133c130206c80a2
jugtuttle <jugtuttle@gmail.com>
2020-05-18 14:08:34 -0700

    updated links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import '../App.css'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,31 +8,45 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Joshua Phelps | Portfolio"
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
+      <>
+      <SEO
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
-      </Layout>
+        <div className='sidenav'>
+          <img src="./cover_photo.jpg" alt="Josh Smiling on Boat" />
+          <a href="https://drive.google.com/file/d/1g5UVJPVuVAleX8PMe6lozMqdHkjzj1jM/view?usp=sharing" target="_blank">Resume</a>
+          <a href="https://github.com/Joshua-Phelps" target="_blank">Github</a>
+          <a href="https://www.linkedin.com/in/joshuaphelps24/" target="_blank">Linkedin</a>
+          <a href="https://medium.com/@jugtuttle" target="_blank">Blog</a>
+        </div>
+
+        <div className='content'>
+        <Layout location={this.props.location} title={siteTitle}>
+
+          
+
+          <h1>
+            Welcome {" "}
+            <span role="img" aria-label="wave emoji">
+              👋
+            </span>
+          </h1>
+          <p>Welcome to my software engineering portfolio.</p>
+          <p>
+            This starter comes out of the box with styled components and Gatsby's
+            default starter blog running on Netlify CMS.
+          </p>
+          <p>Now go build something great!</p>
+          <Link to="/blog/">
+            <Button marginTop="35px">Go to Projects</Button>
+          </Link>
+        </Layout>
+      </div>
+      </>
     )
   }
 }
