f0a9036c39d112238a5aa0033e6f372fd6b9b2ff
jugtuttle <jugtuttle@gmail.com>
2020-09-01 10:57:40 -0700

    updated resume and blog links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
-import '../App.css'
-import coverPhoto from '../images/cover_photo.jpg'
-import Bio from '../components/bio'
+import "../App.css"
+import coverPhoto from "../images/cover_photo.jpg"
+import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
@@ -13,45 +13,78 @@ class IndexPage extends React.Component {
 
     return (
       <>
-      <SEO
+        <SEO
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <div className='sidenav'>
+        <div className="sidenav">
           <img src={coverPhoto} alt="Josh Smiling on Boat" />
-          <a href="https://drive.google.com/file/d/1s4ydcEEQAKi-zrToL4Ba5h1xyAiv6zyu/view?usp=sharing" target="_blank">Resume</a>
-          <a href="https://github.com/Joshua-Phelps" target="_blank">Github</a>
-          <a href="https://www.linkedin.com/in/joshuaphelps24/" target="_blank">Linkedin</a>
-          <a href="https://medium.com/@jugtuttle" target="_blank">Blog</a>
+          <a
+            href="https://docs.google.com/document/d/1oL71HPZDh_4F_9Ea84rF50ZW9PlulK62jViu_QgoiEM/edit?usp=sharing"
+            target="_blank"
+          >
+            Resume
+          </a>
+          <a href="https://github.com/Joshua-Phelps" target="_blank">
+            Github
+          </a>
+          <a href="https://www.linkedin.com/in/joshuaphelps24/" target="_blank">
+            Linkedin
+          </a>
+          <a href="hhttps://medium.com/@joshua.phelps89" target="_blank">
+            Blog
+          </a>
         </div>
 
-        <div className='content'>
-        <Layout location={this.props.location} title={siteTitle}>
-          {/* <h1>
+        <div className="content">
+          <Layout location={this.props.location} title={siteTitle}>
+            {/* <h1>
             Welcome {" "}
             <span role="img" aria-label="wave emoji">
               👋
             </span>
           </h1> */}
-          <h2>
-              About Me
-          </h2>
+            <h2>About Me</h2>
             <Bio />
-          <h2>Project Demos</h2>
-          
-            <div className='grid-container-index'>
+            <h2>Project Demos</h2>
+
+            <div className="grid-container-index">
               <div>
-                <iframe className='demos' width="373" height="210" src="https://www.youtube.com/embed/m9WLhJ9x4ts" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe
+                  className="demos"
+                  width="373"
+                  height="210"
+                  src="https://www.youtube.com/embed/m9WLhJ9x4ts"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
               </div>
-              <iframe className='demos' width="373" height="210" src="https://www.youtube.com/embed/181d-tg6OPc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              <iframe className='demos' width="373" height="210" src="https://www.youtube.com/embed/6zrZ0ikvud4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe
+                className="demos"
+                width="373"
+                height="210"
+                src="https://www.youtube.com/embed/181d-tg6OPc"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+              <iframe
+                className="demos"
+                width="373"
+                height="210"
+                src="https://www.youtube.com/embed/6zrZ0ikvud4"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
             </div>
-       
-          <Link to="/projects/">
-            <Button marginTop="35px">See more Projects</Button>
-          </Link>
-        </Layout>
-      </div>
+
+            <Link to="/projects/">
+              <Button marginTop="35px">See more Projects</Button>
+            </Link>
+          </Layout>
+        </div>
       </>
     )
   }
